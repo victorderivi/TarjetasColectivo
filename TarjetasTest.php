d56414b369907cc900d8dd7f079291bc00ea7670
victorderivi <vderivi@hotmail.com>
2015-09-08 18:48:27 -0300

    Update TarjetasTest.php

--- TarjetasTest.php
+++ TarjetasTest.php
@@ -4,20 +4,20 @@
 
     public function testrecarga()
     {
-      $tarj = new tarjetaComun;
+      $tarj = new Tarjetas.php\tarjetaComun;
       $this->assertEquals(230,$tarj->recarga(196));
     }
     public function testpagarboleto()
     {
-      $bondi1 = new colectivo("Semtur","K",11);
-      $tarj = new tarjetaComun;
+      $bondi1 = new Tarjetas.php\colectivo("Semtur","K",11);
+      $tarj = new Tarjetas.php\tarjetaComun;
       $tarj->recarga(196);
       $this->assertEquals(5.75,$tarj->pagarBoleto($bondi1,1441300000));
     }
 
     public function testsaldo()
     {
-      $tarj = new tarjetaComun;
+      $tarj = new Tarjetas.php\tarjetaComun;
       $tarj->recarga(196);
       $this->assertEquals(230,$tarj->saldo());
     }
